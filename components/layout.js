739a35b29c0f2324d0796d7b701a88a2baad6bd7
Justynr <justynr@me.com>
2021-05-29 15:13:19 -0400

    Add nav bar

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,13 @@
 import Alert from '../components/alert'
 import Footer from '../components/footer'
+import Nav from './Nav'
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <div className="min-h-screen">
         <Alert preview={preview} />
+        <Nav/>
         <main>{children}</main>
       </div>
       <Footer />
--- components/post-header.js
+++ components/post-header.js
@@ -7,9 +7,9 @@ export default function PostHeader({ title, coverImage, date, author }) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
+      {/* <div className="hidden md:block md:mb-12">
         <Avatar name={author.name} picture={author.picture} />
-      </div>
+      </div> */}
       <div className="mb-8 md:mb-16 -mx-5 sm:mx-0">
         <CoverImage
           title={title}

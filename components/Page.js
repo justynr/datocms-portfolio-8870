cfc5a2b0035261da203acb77c30a325e4887f56f
Justynr <justynr@me.com>
2021-05-30 13:38:44 -0400

    styles and fonts

--- components/Page.js
+++ components/Page.js
@@ -1,14 +1,71 @@
-import Alert from './alert';
 import Footer from './footer';
 import Nav from './Nav';
+import PropTypes from 'prop-types';
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'roboto-mono';
+    src: url('https://fonts.googleapis.com/css2?family=Roboto+Mono');
+    /* format:('woff2'); */
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    --darkTheme:#110045;
+    --midDarkTheme:#3C00D5;
+    --middleTheme: #6008F5;
+    --midLightTheme: #DE12F6;
+    --lightTheme:#57CEF3;
+    --red: #ff0000;
+    --lightRed: pink;
+    --orange: #e2b04a;
+    --black: #393939;
+    --grey: #3A3A3A;
+    --gray: var(--grey);
+    --lightGrey: #e1e1e1;
+    --lightGray: var(--lightGrey);
+    --offWhite:(--#ededed);
+    --maxWidth: 1000px;
+    --bs: 0 12px 24px 0 rgba(0,0,0,0.09);
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+  a {
+    text-decoration: none;
+    color: var(--black);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+
+  button {
+    font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--maxWidth);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default function Page({ preview, children }) {
   return (
     <>
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <Nav />
-        <main>{children}</main>
+        <InnerStyles>{children}</InnerStyles>
       </div>
       <Footer />
     </>
--- components/styles/NavStyles.js
+++ components/styles/NavStyles.js
@@ -5,6 +5,7 @@ const NavStyles = styled.ul`
   padding: 0;
   display: flex;
   justify-self: end;
+  justify-content:center;
   font-size: 2rem;
   a,
   button {
@@ -12,8 +13,8 @@ const NavStyles = styled.ul`
     display: flex;
     align-items: center;
     position: relative;
-    text-transform: uppercase;
-    font-weight: 900;
+    text-transform: capitalize;
+    font-weight: 400;
     font-size: 1em;
     background: none;
     border: 0;
@@ -21,17 +22,6 @@ const NavStyles = styled.ul`
     @media (max-width: 700px) {
       font-size: 10px;
       padding: 0 10px;
-    }
-    &:before {
-      content: '';
-      width: 2px;
-      background: var(--lightGray);
-      height: 100%;
-      left: 0;
-      position: absolute;
-      transform: skew(-20deg);
-      top: 0;
-      bottom: 0;
     }
     &:after {
       height: 2px;
